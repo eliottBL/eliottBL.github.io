045e180669c14232ac8d6a753893f9886c8feb82
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-10-03 14:55:20 +0200

    feat: ajout des fonctions pour éviter redondance

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
+function Hide(elementId) {
+    document.getElementById(elementId).className = 'caché';
+};
+function HideAll(ArrayId) {
+    for (let i = 0; i < ArrayId.length; i++) {
+        Hide(ArrayId[i]);
+    }
+}
+function Show(elementId) {
+    document.getElementById(elementId).className = 'visible';
+};
+
+function InitColor(elementIdA, elementIdB) {
+    document.getElementById(elementIdA).style.color = 'black';
+    document.getElementById(elementIdB).style.color = 'black';
+};
+function ChangeColor(elementIdGreen, elementIdBlack) {
+    document.getElementById(elementIdGreen).style.color = 'greenyellow';
+    document.getElementById(elementIdBlack).style.color = 'black';
+};
+
+
 document.querySelector(".about").addEventListener("click", function (event) {
     if (document.getElementById("bio").className === 'caché') {
-        document.getElementById("projet2").className = 'caché';
-        document.getElementById("projet1").className = 'caché';
-        document.getElementById("bio").className = 'visible';
-        document.getElementById("projets").className = 'caché'
+        Show("bio");
+        let ArrayId = ["projet1", "projet2", "projets"];
+        HideAll(ArrayId);
     } else {
-        document.getElementById("bio").className = 'caché';
+        Hide("bio");
     }
 });
 
 document.querySelector(".works").addEventListener("click", function (event) {
     if (document.getElementById("projets").className === 'caché') {
-        document.getElementById("projet2").className = 'caché';
-        document.getElementById("projet1").className = 'caché';
-        document.getElementById("p1").style.color = 'black';
-        document.getElementById("p2").style.color = 'black';
-        document.getElementById("projets").className = 'visible';
-        document.getElementById("bio").className = 'caché';
+        InitColor("p1", "p2");
+        Show("projets");
+        let ArrayId = ["projet1", "projet2", "bio"];
+        HideAll(ArrayId);
     } else {
-        document.getElementById("projets").className = 'caché';
-        document.getElementById("projet2").className = 'caché';
-        document.getElementById("projet1").className = 'caché';
+        let ArrayId = ["projet1", "projet2", "projets"];
+        HideAll(ArrayId);
     }
 });
 
 document.getElementById("p1").addEventListener("click", function (event) {
     if (document.getElementById("projet1").className === 'caché') {
-        document.getElementById("p1").style.color = 'greenyellow';
-        document.getElementById("p2").style.color = 'black';
-        document.getElementById("projet1").className = 'visible';
-        document.getElementById("projet2").className = 'caché';
+        ChangeColor("p1", "p2");
+        Hide("projet2");
+        Show("projet1");
     }
 });
 
 document.getElementById("p2").addEventListener("click", function (event) {
     if (document.getElementById("projet2").className === 'caché') {
-        document.getElementById("p2").style.color = 'greenyellow';
-        document.getElementById("p1").style.color = 'black';
-        document.getElementById("projet2").className = 'visible';
-        document.getElementById("projet1").className = 'caché';
+        ChangeColor("p2", "p1");
+        Hide("projet1");
+        Show("projet2");
     }
 });
